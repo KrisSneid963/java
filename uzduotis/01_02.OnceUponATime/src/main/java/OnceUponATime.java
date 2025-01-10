699b83de849b55b80176f6c24b2ee8f8899c1c1e
Kristina <kristina.sneider@gmail.com>
2025-01-11 00:48:47 +0200

    Add uzduotis 1.2

---
+++ uzduotis/01_02.OnceUponATime/src/main/java/OnceUponATime.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
